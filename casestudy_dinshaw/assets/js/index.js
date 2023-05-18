7b93c2efb91b551e7301aa2895b7958ec52d4b88
ak-ak-k <adityakirtane27@gmail.com>
2023-05-18 19:54:40 +0530

    update

--- casestudy_dinshaw/assets/js/index.js
+++ casestudy_dinshaw/assets/js/index.js
@@ -1,12 +1,41 @@
 function openNav() {
   document.getElementById("mySidenav").style.width = "100%";
-  console.log("clicked");
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
-  console.log("closeNav");
 }
+
+// sidenav
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("click");
+  document.querySelectorAll(".sidebar .nav-link").forEach(function (element) {
+    element.addEventListener("click", function (e) {
+      let nextEl = element.nextElementSibling;
+      let parentEl = element.parentElement;
+      this.classList.toggle("active");
+
+      if (nextEl) {
+        e.preventDefault();
+        let mycollapse = new bootstrap.Collapse(nextEl);
+        if (nextEl.classList.contains("show")) {
+          mycollapse.hide();
+        } else {
+          mycollapse.show();
+          // find other submenus with class=show
+          var opened_submenu =
+            parentEl.parentElement.querySelector(".submenu.show");
+          // if it exists, then close all of them
+          if (opened_submenu) {
+            new bootstrap.Collapse(opened_submenu);
+          }
+        }
+      }
+    }); // addEventListener
+  }); // forEach
+});
+// DOMContentLoaded  end
 
 const accordion = document.getElementsByClassName("container");
 
--- n_howItWorks_merchant/assets/js/index.js
+++ n_howItWorks_merchant/assets/js/index.js
@@ -1,12 +1,43 @@
-function openNav() {
+function openNav()
+{
   document.getElementById("mySidenav").style.width = "100%";
-  document.getElementById("mySidenav").style.overflow = "hidden";
-  console.log("clicked");
 }
 
-function closeNav() {
+function closeNav()
+{
   document.getElementById("mySidenav").style.width = "0";
 }
+
+// sidenav
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("click");
+  document.querySelectorAll(".sidebar .nav-link").forEach(function (element) {
+    element.addEventListener("click", function (e) {
+      let nextEl = element.nextElementSibling;
+      let parentEl = element.parentElement;
+      this.classList.toggle("active");
+
+      if (nextEl) {
+        e.preventDefault();
+        let mycollapse = new bootstrap.Collapse(nextEl);
+        if (nextEl.classList.contains("show")) {
+          mycollapse.hide();
+        } else {
+          mycollapse.show();
+          // find other submenus with class=show
+          var opened_submenu =
+            parentEl.parentElement.querySelector(".submenu.show");
+          // if it exists, then close all of them
+          if (opened_submenu) {
+            new bootstrap.Collapse(opened_submenu);
+          }
+        }
+      }
+    }); // addEventListener
+  }); // forEach
+});
+  // DOMContentLoaded  end
 
 const accordion = document.getElementsByClassName("container");
 
